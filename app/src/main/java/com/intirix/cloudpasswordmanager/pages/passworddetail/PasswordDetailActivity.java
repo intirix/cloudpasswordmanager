f08e57589409be3dbf5d520865cd8b71a56ae0a9
Jeff Mercer <jeff.mercer@gmail.com>
2016-07-26 21:50:59 -0400

    Added logoff scenarios for PasswordDetail page

--- app/src/main/java/com/intirix/cloudpasswordmanager/pages/passworddetail/PasswordDetailActivity.java
+++ app/src/main/java/com/intirix/cloudpasswordmanager/pages/passworddetail/PasswordDetailActivity.java
@@ -1,5 +1,6 @@
 package com.intirix.cloudpasswordmanager.pages.passworddetail;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +8,13 @@ import android.widget.TextView;
 
 import com.intirix.cloudpasswordmanager.PasswordApplication;
 import com.intirix.cloudpasswordmanager.R;
+import com.intirix.cloudpasswordmanager.pages.LoginActivity;
+import com.intirix.cloudpasswordmanager.services.SessionService;
+import com.intirix.cloudpasswordmanager.services.beans.PasswordBean;
+
+import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +22,10 @@ import butterknife.ButterKnife;
 public class PasswordDetailActivity extends AppCompatActivity {
 
     public static String KEY_PASSWORD_INDEX = "PasswordIndex";
+
+
+    @Inject
+    SessionService sessionService;
 
     @BindView(R.id.password_detail_website)
     TextView website;
@@ -51,6 +63,8 @@ public class PasswordDetailActivity extends AppCompatActivity {
     @BindView(R.id.password_detail_notes_value)
     TextView notes;
 
+    PasswordBean passwordBean;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,5 +76,26 @@ public class PasswordDetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         PasswordApplication.getSInjector(this).inject(this);
 
+        int passwordIndex = getIntent().getIntExtra(KEY_PASSWORD_INDEX, 0);
+
+        if (sessionService.getCurrentSession()==null) {
+            logoff();
+        } else {
+            List<PasswordBean> passwordBeanList = sessionService.getCurrentSession().getPasswordBeanList();
+
+            if (passwordBeanList==null || passwordBeanList.size()<passwordIndex) {
+                logoff();
+            } else {
+                passwordBean = passwordBeanList.get(passwordIndex);
+            }
+        }
+
+    }
+
+    protected void logoff() {
+        sessionService.end();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
---
+++ app/src/test/java/com/intirix/cloudpasswordmanager/pages/passworddetail/PasswordDetailsActivityActionSpec.java
@@ -0,0 +1,116 @@
+package com.intirix.cloudpasswordmanager.pages.passworddetail;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.intirix.cloudpasswordmanager.BaseTestCase;
+import com.intirix.cloudpasswordmanager.BuildConfig;
+import com.intirix.cloudpasswordmanager.TestPasswordApplication;
+import com.intirix.cloudpasswordmanager.pages.LoginActivity;
+import com.intirix.cloudpasswordmanager.services.SessionService;
+import com.intirix.cloudpasswordmanager.services.beans.PasswordBean;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.util.ActivityController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jeff on 6/19/16.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class,
+        application = TestPasswordApplication.class)
+public class PasswordDetailsActivityActionSpec extends BaseTestCase {
+
+    @Test
+    public void verifyNoIntentLogsUserOff() throws Exception {
+        ActivityController<PasswordDetailActivity> controller = Robolectric.buildActivity(PasswordDetailActivity.class).create().start().resume();
+        PasswordDetailActivity activity = controller.get();
+
+        controller.pause().stop().destroy();
+
+        assertLogOff(activity);
+    }
+
+
+    @Test
+    public void verifyEmptySessionLogsUserOff() throws Exception {
+        Intent intent = new Intent();
+        intent.putExtra(PasswordDetailActivity.KEY_PASSWORD_INDEX, 0);
+        ActivityController<PasswordDetailActivity> controller = Robolectric.buildActivity(PasswordDetailActivity.class).withIntent(intent).create().start().resume();
+        PasswordDetailActivity activity = controller.get();
+
+        controller.pause().stop().destroy();
+
+        assertLogOff(activity);
+    }
+
+
+    @Test
+    public void verifyInvalidIndexLogsUserOff() throws Exception {
+
+
+        SessionService sessionService = serviceRef.sessionService();
+
+        List<PasswordBean> passwords = new ArrayList<>();
+
+        sessionService.start();
+        sessionService.getCurrentSession().setPasswordBeanList(passwords);
+
+        Intent intent = new Intent();
+        intent.putExtra(PasswordDetailActivity.KEY_PASSWORD_INDEX, 1);
+        ActivityController<PasswordDetailActivity> controller = Robolectric.buildActivity(PasswordDetailActivity.class).withIntent(intent).create().start().resume();
+        PasswordDetailActivity activity = controller.get();
+
+        controller.pause().stop().destroy();
+
+        assertLogOff(activity);
+    }
+
+
+    @Test
+    public void verifyValidPassword() throws Exception {
+
+
+        SessionService sessionService = serviceRef.sessionService();
+
+        List<PasswordBean> passwords = new ArrayList<>();
+
+        PasswordBean bean = new PasswordBean();
+        passwords.add(bean);
+
+        sessionService.start();
+        sessionService.getCurrentSession().setPasswordBeanList(passwords);
+
+        Intent intent = new Intent();
+        intent.putExtra(PasswordDetailActivity.KEY_PASSWORD_INDEX, 0);
+        ActivityController<PasswordDetailActivity> controller = Robolectric.buildActivity(PasswordDetailActivity.class).withIntent(intent).create().start().resume();
+        PasswordDetailActivity activity = controller.get();
+
+        controller.pause().stop().destroy();
+
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent nextIntent = shadowActivity.peekNextStartedActivity();
+        Assert.assertNull("We expected stay on this activity, but are not", nextIntent);
+    }
+
+
+    protected void assertLogOff(Activity activity) {
+        // verify that we are starting the LoginActivity
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent intent = shadowActivity.peekNextStartedActivity();
+        Assert.assertNotNull("We expected to change activity, but are not", intent);
+        Assert.assertEquals(LoginActivity.class.getName(), intent.getComponent().getClassName());
+        Assert.assertEquals(Intent.FLAG_ACTIVITY_CLEAR_TASK, intent.getFlags() & Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Assert.assertEquals(Intent.FLAG_ACTIVITY_NEW_TASK, intent.getFlags() & Intent.FLAG_ACTIVITY_NEW_TASK);
+    }
+}
